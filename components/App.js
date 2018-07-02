957e7f7f4d7a85e545fa468605df206aee300523
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-07-02 22:18:05 +0200

    add components, JS, html

---
+++ components/App.js
@@ -0,0 +1,37 @@
+var contacts = [
+{
+	id: 1,
+	firstName: 'Jan',
+	lastName: 'Nowak',
+	email: 'jan.nowak@example.com',
+},
+{
+	id: 2,
+	firstName: 'Jon',
+	lastName: 'Steward',
+	email: 'jon.sterwad@example.com',
+},
+{
+	id: 3,
+	firstName: 'Zbigniew',
+	lastName: 'Kozioł',
+	email: 'zbigniew.koziol@example.com',
+}
+];
+
+var contactForm = {
+	firstName: "",
+	lastName: "",
+	email: ""
+};
+
+var App = React.createClass({
+	render: function() {
+		return (
+          	<div className={'app'}>
+            <ContactForm contact={contactForm}></ContactForm>
+            <Contacts items = {contacts}></Contacts>
+            </div>
+		);
+	}
+});
